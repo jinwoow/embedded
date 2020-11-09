66a51baad51bef7e98e5b83b687326cbe04215a8
jinwoo lee <qkrtjrtns21@naver.com>
2020-11-09 22:26:35 +0900

    led55

---
+++ ledtest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include "led.h"
+
+int main()
+{
+	ledLibInit();
+	ledStatus();
+	sleep(3);
+	ledLibExit();
+}
