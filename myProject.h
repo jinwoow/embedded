766e7e8bca27a831075763412e956ba47b9bffdd
kimdo0309 <kimdo0309@mju.ac.kr>
2020-11-14 19:33:45 +0900

    GOGO
    GOGO

--- myProject.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MYPROJECT_H_
-#define _MYPROJECT_H_
-
-void myProgect(void);
-
-#endif
---
+++ peri/buttontest.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include "button.h"
+#include <sys/ipc.h>
+
+#define INPUT_DEVICE_LIST "dev/input/event"
+#define PROBE_FILE "/proc/bus/input/devices"
+
+#define INPUT_DEVICE_LIST "/dev/int/event"
+#define PROBE_FILE "proc/bus/input/devices"
+#define HAVE_TO_FIND_1 "N:NAME=\"ecube-button\"\n"
+#define HAVE_TO_FIND_2 "H:Handlers =kbd event"
+
+int main(int argc, char *argv[])
+{
+	char inputDevPath[200]={0,};
+	int fp;
+    BUTTON_MSG_T RxData;
+	while(1){
+		buttonInit();
+		int readsize;
+	    int returnValue =0;
+	    int msgID= msgget((key_t)MESSAGE_ID,IPC_CREAT|0666);
+    	returnValue= msgrcv(msgID,&RxData,sizeof(RxData.keyinput),0,IPC_NOWAIT);
+    	if(returnValue==-1){
+			break;
+		}
+		else{
+	}
+	}
+}
