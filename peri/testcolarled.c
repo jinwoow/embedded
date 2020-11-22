05679bf735965709da5e0370cc502c8c8b9dcba2
kimdo0309 <kimdo0309@mju.ac.kr>
2020-11-22 23:49:23 +0900

    colarled

---
+++ peri/testcolarled.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h> // for open/close
+#include <fcntl.h>
+
+int main(){
+	colarlcd(50,20,30);
+}
