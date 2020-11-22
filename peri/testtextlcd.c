e25fcc1a38f955b86e0f85583678c281cb9d36a8
kimdo0309 <kimdo0309@mju.ac.kr>
2020-11-22 23:31:13 +0900

    textLCD

---
+++ peri/testtextlcd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h> // for open/close
+#include <fcntl.h> // for O_RDWR
+
+
+int main(){
+	textlcd(1,'hello');
+}
