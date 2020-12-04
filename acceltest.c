be908f0da362a2355af3f6a1ef00f575f86fddae
jinwoo lee <qkrtjrtns21@naver.com>
2020-12-04 14:28:47 +0900

    자이로 센

---
+++ acceltest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "accelMagGyro.h"
+
+int main(void)
+{
+    int Acceler();
+    int Magnet();
+    int Gyroscope();
+}
