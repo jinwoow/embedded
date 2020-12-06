11232926287ebcb519fd866fa62cdfd614773fad
kimdo0309 <kimdo0309@mju.ac.kr>
2020-12-06 23:59:04 +0900

    d

--- main.c
+++ main.c
@@ -46,7 +46,60 @@ void* DoSomeThing(void *arg)
 	int msgID = msgget (MESSAGE_ID, IPC_CREAT|0666);//메시지큐
 	
 	//lseek(fdpower,(off_t)0,SEEK_SET);
+	textlcd("1","1.item upgrade");
+	textlcd("2","2.exit");
+	int screen_width;
+    int screen_height;
+   	int bits_per_pixel;
+   	int line_length;
+   	int cols = 0, rows = 0;
+	char *data;
+	//FrameBuffer init
+   	if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
+	{		
+		printf ("FrameBuffer Init Failed\r\n");
+		return 0;
+	}
 	
+	int conFD = open ("/dev/tty0", O_RDWR);
+	ioctl(conFD, KDSETMODE, KD_GRAPHICS);
+	close (conFD);
+		
+	//Clear FB.
+	fb_clear();
+	
+	//FileRead
+	int error=0;
+	struct jpeg_decompress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_decompress(&cinfo);
+	FILE *fp = fopen("test.jpg", "rb");
+	jpeg_stdio_src(&cinfo, fp);
+	jpeg_read_header(&cinfo, TRUE); 
+	//printf ("JPG %d by %d by %d, %d\n",
+	//	cinfo.image_width,cinfo.image_height,cinfo.num_components, cinfo.output_scanline);
+	cols = cinfo.image_width;
+	rows = cinfo.image_height;
+		
+	data = malloc(cols*rows*3);
+	int currPoint = 0;
+	jpeg_start_decompress(&cinfo);
+	while(cinfo.output_scanline < cinfo.output_height) 
+	{
+		//printf ("CInfoScanlines:%d\r\n",cinfo.output_scanline);
+		char *tempPtr=&data[currPoint];
+		jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&tempPtr, 1);
+		currPoint+=cols*3;
+	}
+	jpeg_finish_decompress(&cinfo);
+	jpeg_destroy_decompress(&cinfo);
+	fclose(fp);
+
+	fb_write_reverse(data, cols,rows);
+	free(data);
+
+	fb_close();
 	while(1)
 	{
 		//printf("무엇을 선택하겠는가? ");
@@ -120,29 +173,10 @@ void* DoSomeThing(void *arg)
 										textlcd("2","item upgrade");
 										sleep(1);
 										itemup();
-										textlcd("1","1.item upgrade");
-										textlcd("2","2.exit");
-	//FrameBuffer init
-   	if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
-	{		
-		printf ("FrameBuffer Init Failed\r\n");
-		return 0;
-	}
-	
-	int conFD = open ("/dev/tty0", O_RDWR);
-	ioctl(conFD, KDSETMODE, KD_GRAPHICS);
-	close (conFD);
-		
-	//Clear FB.
-	fb_clear();
-	
+										msgRx.keyInput=0;
+										returnValue=0;
 	//FileRead
-	int error=0;
-	struct jpeg_decompress_struct cinfo;
-	struct jpeg_error_mgr jerr;
-	cinfo.err = jpeg_std_error(&jerr);
-	jpeg_create_decompress(&cinfo);
-	FILE *fp = fopen("test.jpg", "rb");
+	FILE *fk = fopen("test.jpg", "rb");
 	jpeg_stdio_src(&cinfo, fp);
 	jpeg_read_header(&cinfo, TRUE); 
 	//printf ("JPG %d by %d by %d, %d\n",
@@ -151,7 +185,6 @@ void* DoSomeThing(void *arg)
 	rows = cinfo.image_height;
 		
 	data = malloc(cols*rows*3);
-	int currPoint = 0;
 	jpeg_start_decompress(&cinfo);
 	while(cinfo.output_scanline < cinfo.output_height) 
 	{
@@ -162,7 +195,7 @@ void* DoSomeThing(void *arg)
 	}
 	jpeg_finish_decompress(&cinfo);
 	jpeg_destroy_decompress(&cinfo);
-	fclose(fp);
+	fclose(fk);
 
 	fb_write_reverse(data, cols,rows);
 	free(data);
