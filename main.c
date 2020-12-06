ecda8e98e2d57536d1a09d6c5e4c37c5bca0d0f5
kimdo0309 <kimdo0309@mju.ac.kr>
2020-12-07 01:26:01 +0900

    dd

--- main.c
+++ main.c
@@ -102,54 +102,6 @@ void* DoSomeThing(void *arg)
 	free(data);
 
 	fb_close();
-	sleep(3);
-	//FrameBuffer init
-   										if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
-										{		
-											printf ("FrameBuffer Init Failed\r\n");
-											return 0;
-										}
-	
-										conFD = open ("/dev/tty0", O_RDWR);
-										ioctl(conFD, KDSETMODE, KD_GRAPHICS);
-										close (conFD);
-		
-										//Clear FB.
-										fb_clear();
-	
-										//FileRead
-										error=0;
-										cinfo.err = jpeg_std_error(&jerr);
-										jpeg_create_decompress(&cinfo);
-	
-	
-										fp = fopen("upgrade.jpg", "rb");
-										jpeg_stdio_src(&cinfo, fp);
-										jpeg_read_header(&cinfo, TRUE); 
-										//printf ("JPG %d by %d by %d, %d\n",
-										//	cinfo.image_width,cinfo.image_height,cinfo.num_components, cinfo.output_scanline);
-										cols = cinfo.image_width;
-										rows = cinfo.image_height;
-																				
-										data = malloc(cols*rows*3);
-										currPoint = 0;
-										jpeg_start_decompress(&cinfo);
-										while(cinfo.output_scanline < cinfo.output_height) 
-										{
-											//printf ("CInfoScanlines:%d\r\n",cinfo.output_scanline);
-											char *tempPtr=&data[currPoint];
-											jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&tempPtr, 1);
-											currPoint+=cols*3;
-										}
-										jpeg_finish_decompress(&cinfo);
-										jpeg_destroy_decompress(&cinfo);
-										fclose(fp);
-										fb_write_reverse(data, cols,rows);
-										free(data);
-
-										fb_close();
-										textlcd("1","1.item upgrade");
-										textlcd("2","2.exit");
 
 
 	while(1)
@@ -178,60 +130,61 @@ void* DoSomeThing(void *arg)
 					case KEY_VOLUMEDOWN:
 										textlcd("1","input");
 										textlcd("2","item upgrade");
+										sleep(1);
 											//FrameBuffer init
  									   int screen_width;
-    int screen_height;
-   	int bits_per_pixel;
-   	int line_length;
-   	int cols = 0, rows = 0;
-	char *data;
-	//FrameBuffer init
-   	if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
-	{		
-		printf ("FrameBuffer Init Failed\r\n");
-		return 0;
-	}
+										int screen_height;
+										int bits_per_pixel;
+   									   int line_length;
+   									   int cols = 0, rows = 0;
+									   char *data;
+									   //FrameBuffer init
+										if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
+									   {		
+									   	printf ("FrameBuffer Init Failed\r\n");
+									   	return 0;
+									   }
 	
-	int conFD = open ("/dev/tty0", O_RDWR);
-	ioctl(conFD, KDSETMODE, KD_GRAPHICS);
-	close (conFD);
+									   int conFD = open ("/dev/tty0", O_RDWR);
+									   ioctl(conFD, KDSETMODE, KD_GRAPHICS);
+									   close (conFD);
 		
-	//Clear FB.
-	fb_clear();
+									   //Clear FB.
+									   fb_clear();
 	
-	//FileRead
-	int error=0;
-	struct jpeg_decompress_struct cinfo;
-	struct jpeg_error_mgr jerr;
-	cinfo.err = jpeg_std_error(&jerr);
-	jpeg_create_decompress(&cinfo);
+									   //FileRead
+									   int error=0;
+									   struct jpeg_decompress_struct cinfo;
+									   struct jpeg_error_mgr jerr;
+									   cinfo.err = jpeg_std_error(&jerr);
+									   jpeg_create_decompress(&cinfo);
 	
 	
-	FILE *fp = fopen("upgrade.jpg", "rb");
-	jpeg_stdio_src(&cinfo, fp);
-	jpeg_read_header(&cinfo, TRUE); 
-	//printf ("JPG %d by %d by %d, %d\n",
-	//	cinfo.image_width,cinfo.image_height,cinfo.num_components, cinfo.output_scanline);
-	cols = cinfo.image_width;
-	rows = cinfo.image_height;
+									   FILE *fp = fopen("upgrade.jpg", "rb");
+									   jpeg_stdio_src(&cinfo, fp);
+									   jpeg_read_header(&cinfo, TRUE); 
+									   //printf ("JPG %d by %d by %d, %d\n",
+									   //	cinfo.image_width,cinfo.image_height,cinfo.num_components, cinfo.output_scanline);
+									   cols = cinfo.image_width;
+									   rows = cinfo.image_height;
 		
-	data = malloc(cols*rows*3);
-	int currPoint = 0;
-	jpeg_start_decompress(&cinfo);
-	while(cinfo.output_scanline < cinfo.output_height) 
-	{
-		//printf ("CInfoScanlines:%d\r\n",cinfo.output_scanline);
-		char *tempPtr=&data[currPoint];
-		jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&tempPtr, 1);
-		currPoint+=cols*3;
-	}
-	jpeg_finish_decompress(&cinfo);
-	jpeg_destroy_decompress(&cinfo);
-	fclose(fp);
-	fb_write_reverse(data, cols,rows);
-	free(data);
+									   data = malloc(cols*rows*3);
+									   int currPoint = 0;
+									   jpeg_start_decompress(&cinfo);
+									   while(cinfo.output_scanline < cinfo.output_height) 
+									   {
+									   	//printf ("CInfoScanlines:%d\r\n",cinfo.output_scanline);
+									   	char *tempPtr=&data[currPoint];
+									   	jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&tempPtr, 1);
+									   	currPoint+=cols*3;
+									   }
+										jpeg_finish_decompress(&cinfo);
+									   jpeg_destroy_decompress(&cinfo);
+									   fclose(fp);
+									   fb_write_reverse(data, cols,rows);
+									   free(data);
 
-	fb_close();
+									   fb_close();
 										
 										itemup();
 									
