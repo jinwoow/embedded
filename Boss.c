e918b486fc4cf1e2bb36c508cb3ae477a308d793
kimdo0309 <kimdo0309@mju.ac.kr>
2020-12-07 18:44:17 +0900

    dd

--- Boss.c
+++ Boss.c
@@ -13,8 +13,7 @@
 #define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/"
 #define MAGNEPATH "/sys/class/misc/FreescaleMagnetometer/"
 #define GYROPATH "/sys/class/misc/FreescaleGyroscope/"
-#include "../libfbdev/libfbdev.h"
-#include "../libjpeg/jpeglib.h"
+
 
 int Boss()
 {
@@ -28,59 +27,7 @@ int Boss()
 	int cnt=0;
 	char buff[6];
 	int BossHP=30000;
-	int screen_width;
-    int screen_height;
-   	int bits_per_pixel;
-   	int line_length;
-   	int cols = 0, rows = 0;
-	char *data;
-	//FrameBuffer init
-   	if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
-	{		
-		printf ("FrameBuffer Init Failed\r\n");
-		return 0;
-	}
 	
-	int conFD = open ("/dev/tty0", O_RDWR);
-	ioctl(conFD, KDSETMODE, KD_GRAPHICS);
-	close (conFD);
-		
-	//Clear FB.
-	fb_clear();
-	
-	//FileRead
-	int error=0;
-	struct jpeg_decompress_struct cinfo;
-	struct jpeg_error_mgr jerr;
-	cinfo.err = jpeg_std_error(&jerr);
-	jpeg_create_decompress(&cinfo);
-	
-	
-	fp = fopen("boss.jpg", "rb");
-	jpeg_stdio_src(&cinfo, fp);
-	jpeg_read_header(&cinfo, TRUE); 
-	//printf ("JPG %d by %d by %d, %d\n",
-	//	cinfo.image_width,cinfo.image_height,cinfo.num_components, cinfo.output_scanline);
-	cols = cinfo.image_width;
-	rows = cinfo.image_height;
-		
-	data = malloc(cols*rows*3);
-	int currPoint = 0;
-	jpeg_start_decompress(&cinfo);
-	while(cinfo.output_scanline < cinfo.output_height) 
-	{
-		//printf ("CInfoScanlines:%d\r\n",cinfo.output_scanline);
-		char *tempPtr=&data[currPoint];
-		jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&tempPtr, 1);
-		currPoint+=cols*3;
-	}
-	jpeg_finish_decompress(&cinfo);
-	jpeg_destroy_decompress(&cinfo);
-	fclose(fp);
-	fb_write_reverse(data, cols,rows);
-	free(data);
-
-	fb_close();
 	
 	//Accelerometer
 	fd = open (ACCELPATH "enable",O_WRONLY);
@@ -221,51 +168,7 @@ int Boss()
 		else
 		;
 	}
-										//FrameBuffer init
-   										if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 )
-										{		
-											printf ("FrameBuffer Init Failed\r\n");
-											return 0;
-										}
-	
-										conFD = open ("/dev/tty0", O_RDWR);
-										ioctl(conFD, KDSETMODE, KD_GRAPHICS);
-										close (conFD);
-		
-										//Clear FB.
-										fb_clear();
-	
-										//FileRead
-										error=0;
-										cinfo.err = jpeg_std_error(&jerr);
-										jpeg_create_decompress(&cinfo);
-	
-	
-										fp = fopen("test.jpg", "rb");
-										jpeg_stdio_src(&cinfo, fp);
-										jpeg_read_header(&cinfo, TRUE); 
-										//printf ("JPG %d by %d by %d, %d\n",
-										//	cinfo.image_width,cinfo.image_height,cinfo.num_components, cinfo.output_scanline);
-										cols = cinfo.image_width;
-										rows = cinfo.image_height;
-																				
-										data = malloc(cols*rows*3);
-										currPoint = 0;
-										jpeg_start_decompress(&cinfo);
-										while(cinfo.output_scanline < cinfo.output_height) 
-										{
-											//printf ("CInfoScanlines:%d\r\n",cinfo.output_scanline);
-											char *tempPtr=&data[currPoint];
-											jpeg_read_scanlines(&cinfo, (JSAMPARRAY)&tempPtr, 1);
-											currPoint+=cols*3;
-										}
-										jpeg_finish_decompress(&cinfo);
-										jpeg_destroy_decompress(&cinfo);
-										fclose(fp);
-										fb_write_reverse(data, cols,rows);
-										free(data);
 
-										fb_close();
 	buzzerExit();
 	ledLibExit();
 	return 0;
